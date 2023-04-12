3945191d8bfeae8bce82defb5609994bb003f0ee
huuthao98 <menofthewind@gmail.com>
2023-04-12 17:27:05 +0700

    build button

---
+++ src/components/Button/index.js
@@ -0,0 +1,62 @@
+// import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import styles from './Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    text = false,
+    disabled = false,
+    rounded = false,
+    small = false,
+    large = false,
+    className,
+    onClick,
+    leftIcon,
+    rightIcon,
+    children,
+    ...passProps
+}) {
+    let Comp = 'button';
+    const props = {
+        onClick,
+        ...passProps,
+    };
+
+    if (disabled) {
+        delete props.onClick;
+    }
+
+    const classes = cx('wrapper', {
+        [className]: className,
+        primary,
+        outline,
+        text,
+        disabled,
+        rounded,
+        small,
+        large,
+    });
+
+    if (to) {
+        props.to = to;
+        Comp = Link;
+    } else {
+        props.href = href;
+        Comp = 'a';
+    }
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+        </Comp>
+    );
+}
+
+export default Button;
